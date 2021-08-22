7cceed72c5f297585187da761fb8597b98571a07
Xiebloom <xiekaihuaaa@yahoo.co.jp>
2021-08-23 00:08:59 +0800

    router

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
+import Home from './components/Home'
+import About from './components/About'
+import {NavLink, Route} from 'react-router-dom'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <div class="row">
+          <div class="col-xs-offset-2 col-xs-8">
+            <div class="page-header"><h2>React Router Demo</h2></div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-xs-2 col-xs-offset-2">
+            <div class="list-group">
+              <NavLink activeClassName="myActive" className="list-group-item" to='/home'>Home</NavLink>
+              <NavLink className="list-group-item" to='/about'>About</NavLink>
+            </div>
+          </div>
+          <div class="col-xs-6">
+            <div class="panel">
+              <div class="panel-body">
+                <Route path='/home' component={Home} />
+                <Route path='/about' component={About} />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
 }
-
-export default App;
---
+++ src/components/About/index.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+// import '../../bootstrap.css'
+
+
+export default class About extends Component {
+    render() {
+        return (
+            <div>
+                <h3>我是About的内容</h3>
+            </div>
+        )
+    }
+}
